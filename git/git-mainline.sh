45af1bb884a9b00b182db079749a4452a627b54e
Petr Kozelka <pkozelka@gmail.com>
2015-10-05 23:33:22 +0200

    mainline follower

---
+++ git/git-mainline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Follow main line of the commits between two revs
+# The purpose is to gather docs, consisting of both merge requests AND direct commits
+#
+# TODO ::: not working yet
+
+function filterMainLine() {
+    local hash parentHash
+    read hash parentHash
+    while true
+    do
+        echo "x:$hash"
+        local firstParent="$parentHash"
+        # skip until we find hash==firstParent
+        while [ "$hash" != "$firstParent" ]
+        do
+            read hash parentHash || break
+        done
+    done
+}
+
+git rev-list --parents HEAD | filterMainLine
