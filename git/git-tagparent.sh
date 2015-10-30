47084052bc828e8db8052af426324485e755ff10
Petr Kozelka <pkozelka@gmail.com>
2015-10-30 14:45:34 +0100

    tag parent tagger

---
+++ git/git-tagparent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Finds all tags matching a pattern, and tags its parent with similar tag name (using given prefix).
+# Useful when releases are tagged in side branches.
+#
+
+function markTagParent() {
+    local findPattern=${1?'specify tag prefix'}
+    local addPrefix=${2-'RELEASE-'}
+
+    for tag in $(git tag "${findPattern}"); do
+        local parent=$(git rev-list --max-count=2 | tail -1)
+        git tag "${addPrefix}${tag}" "$parent"
+    done
+}
+
+markTagParent "$@"
