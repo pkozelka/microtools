555f59e90ca22c95fcce57f2d4ecfb20f813bb96
Petr Kozelka <pkozelka@gmail.com>
2016-02-03 07:48:54 +0100

    individual class renames

--- java/rename.sh
+++ java/rename.sh
@@ -7,6 +7,13 @@ function rename() {
     local className=$2
 }
 
+##
+# Filters control file into pairs of NEW,OLD package per line
+#
+function readControlFile() {
+    sed '/^#/d;/^$/d;s:=: :' $CONTROLFILE
+}
+
 function javaFixImports() {
     local javaFile=$1
     # rename
@@ -17,22 +24,44 @@ function javaFixImports() {
     # fix class references if package is imported
 }
 
-function renameAllClasses() {
-    local controlFile='rename.txt'
+function refactorAllClasses() {
     # prepare sed scripts
-    sed 's:=: :' "$controlFile" | while read newClass oldClass; do
+    readControlFile | while read newClass oldClass; do
         printf "s:${oldClass//./\.}:${newClass}:g;\n" >>$TMP/java-fix-imports.sed
     done
     # process all java files
     find * -name '*.java' | while read file; do
         javaFixImports "$file" || return 1
     done
+    # TODO
+}
+
+function doFileRenames() {
+    # translate control file into SED
+    #   which generates "mv" arg pairs, one per line, for piped `find`
+
+    # first, just individual class renames
+    local new old
+    readControlFile | while read new old; do
+        local newClass="${new/#*\./}"
+        [ -z "$newClass" ] && continue
+        local newPackage="${new%.*}"
+        local oldClass="${old/#*\./}"
+        local oldPackage="${old%.*}"
+        case "$oldClass" in
+        '') oldClass="$newClass"; old="$old$newClass";;
+        esac
+        printf "$oldPackage:$oldClass --> $newPackage:$newClass :SED: "
+        printf '\n'
+    done
 }
 
 #### MAIN ####
+
+CONTROLFILE=${1-'rename.txt'}
 
 TMP=/tmp/refactor
 rm -rf $TMP
 mkdir -p $TMP
 
-renameAllClasses
+doFileRenames "$CONTROLFILE"
