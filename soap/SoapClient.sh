9f0a8b847b223f78ea7de10b0e6d37c3abaf14e2
Petr Kozelka <pkozelka@gmail.com>
2016-06-09 11:41:33 +0200

    extracted HttpCall function

--- soap/SoapClient.sh
+++ soap/SoapClient.sh
@@ -10,45 +10,71 @@
 #
 [ -s "SoapClient.config" ] && source "SoapClient.config"
 
+##
+# Performs low-level execution on the given endpoint.
+# @param stdin - the request body
+# @param operationName
+# @file $TMP/${operationName}.request stored request
+# @file $TMP/${operationName}.response stored response
+# @file $TMP/${operationName}.response.code stored http code
+# @returns curl's exitcode, or 99 to indicate non-ok HTTP code (2??)
+#
+function HttpCall() {
+    local operationName="$1"
+    cat >"$TMP/${operationName}.request"
+#    echo $CURL "-d@$TMP/${operationName}.request" --output "$TMP/${operationName}.response" --write-out "%{http_code}" >"$TMP/${operationName}.response.code" >&2
+    $CURL -d@"$TMP/${operationName}.request" --output "$TMP/${operationName}.response" --write-out "%{http_code}" >"$TMP/${operationName}.response.code"
+    local rv="$?"
+    case "$rv" in
+    0);;
+    *)
+        echo "ERROR: curl failed - exitcode is $rv" >&2
+        return $rv;;
+    esac
+    local http_code=$(cat "$TMP/${operationName}.response.code")
+    case "$http_code" in
+    2??);;
+    *)
+        echo "ERROR: server returned HTTP $http_code" >&2;
+        return 99;;
+    esac
+}
+
 function SoapCall() {
     local operationName="${1?'Please specify SOAP operation to call'}"
     shift
     #TODO: finetune processing of both request and response
-    SOAP_OPERATION="$operationName"
-    local soapRequestFunction="SOAP_${operationName}Request"
-    local soapResponseFunction="SOAP_${operationName}Response"
-    if ! $soapRequestFunction "$@" >"$TMP/${operationName}.request"; then
-        echo "ERROR: failed to execute '$soapRequestFunction'" >&2
-        return 1
-    fi
+    case "$operationName" in
+    RAW)
+        cat >"$TMP/${operationName}.request.prepared"
+        ;;
+    *)
+        SOAP_OPERATION="$operationName"
+        local soapRequestFunction="SOAP_${operationName}Request"
+        if ! $soapRequestFunction "$@" >"$TMP/${operationName}.request.prepared"; then
+            echo "ERROR: failed to execute '$soapRequestFunction'" >&2
+            SOAP_OPERATION=""
+            return 1
+        fi
+        SOAP_OPERATION=""
+        ;;
+    esac
     if "$DEBUG"; then
         # just show the request on console
-        cat "$TMP/${operationName}.request"
+        cat "$TMP/${operationName}.request.prepared"
         return 0
     fi
 
     # invoke service
-    $CURL "-d@$TMP/${operationName}.request" --output "$TMP/${operationName}.response" --write-out "%{http_code}" >"$TMP/${operationName}.response.code"
-    local rv="$?"
-    case "$rv" in
-    0);;
-    *) echo "ERROR: curl failed - exitcode is $rv" >&2;return 1;;
-    esac
+    HttpCall "$operationName" < "$TMP/${operationName}.request.prepared" || return 1
 
     # process response status and data
     SoapClient_processResponse "$TMP/${operationName}.response" "$TMP/${operationName}.payload" || return 1
 
-    local http_code=$(cat "$TMP/${operationName}.response.code")
-    case "$http_code" in
-    2??);;
-    *) echo "ERROR: server returned HTTP $http_code" >&2;return 1;;
-    esac
 
+    local soapResponseFunction="SOAP_${operationName}Response"
     type -t "$soapResponseFunction" || soapResponseFunction="cat"
     eval $soapResponseFunction < "$TMP/${operationName}.payload"
-    local rv="$?"
-    SOAP_OPERATION=""
-    return "$rv"
 }
 
 function SoapClient_processResponse() {
@@ -139,11 +165,17 @@ EOF
 # Wraps the payload with a SOAP request.
 #
 function SoapRequest() {
-    local requestElement="${SOAP_OPERATION?'No soap operation in progress!'}Request"
+    local requestElement
     if [ "$1" == "--element" ]; then
         requestElement="${2}"
         shift 2
+    elif [ -n "$SOAP_OPERATION" ]; then
+        requestElement="${SOAP_OPERATION}Request"
+    else
+        echo "ERROR: No SOAP operation in progress" >&2
+        return 1
     fi
+
     #TODO: allow headers to come via options?
     cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -226,7 +258,7 @@ function SoapClient() {
 
     CURL="curl -s -k ${CURL_AUTH} ${REMOTE_URL}${ENDPOINT_URI}"
     if $showWsdl; then
-        ${CURL_GET}${ENDPOINT_WSDL} | xmllint --format - || exit 1
+        ${CURL}${ENDPOINT_WSDL} | xmllint --format - || exit 1
         return 0
     fi
     CURL="$CURL --header Content-Type:text/xml;charset=UTF-8 -X POST"
@@ -242,7 +274,7 @@ function SoapClient() {
     if [ "$command" == "-" ]; then
         # absorb stdin and pass it to the endpoint
         $DEBUG && echo "$CURL -d@-" >&2
-        $CURL_POST -d@-
+        $CURL -d@-
     elif grep -q "^function CMD_$command()" "$0"; then
         # execute the recognized subcommand by calling its function
         CMD_$command "$@"
