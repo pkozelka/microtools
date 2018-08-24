ee39e4d59b36574b5af4ae7f83191e0b613306cb
Petr Kozelka <pkozelka@gmail.com>
2018-08-24 14:07:19 +0200

    setjava: remove non-existing directories when on the PATH modification

--- java/setjava.sh
+++ java/setjava.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+## Set given directory as the new (and only!) java home in both JAVA_HOME and PATH.
+## This script needs to be sourced.
+
 JH="${1?'please specify desired JAVA_HOME'}"
 JHB="$JH/bin"
 
 export JAVA_HOME="$JH"
 
-# TODO: remove any paths that contain java or do not exist
+# remove any paths that contain java or do not exist
 __NEW_PATH="$JHB"
 for i in ${PATH//:/ }; do
-	if [ -x "$i/java" ]; then
+	if ! [ -d "$i" ]; then
+		echo "!! $i" >&2
+	elif [ -x "$i/java" ]; then
 		echo "-- $i" >&2
 	else
 #		echo "   $i" >&2
