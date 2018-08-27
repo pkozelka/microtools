55986c46ffbef9836a9e6fdab293f06c4840f398
Petr Kozelka <pkozelka@gmail.com>
2018-08-27 11:34:07 +0200

    git-gone: added options -d and -D to easily remove gone branches

--- git/git-gone.sh
+++ git/git-gone.sh
@@ -1,1 +1,11 @@
-git branch -v --no-abbrev | sed -n '/^  .* [0-9a-f]\{40\} \[gone\] /{s:  \([^[:space:]]*\) .*:\1:;p;}'
+#!/bin/bash
+
+function listBranches() {
+	git branch -v --no-abbrev | sed -n '/^  .* [0-9a-f]\{40\} \[gone\] /{s:  \([^[:space:]]*\) .*:\1:;p;}'
+}
+
+case "$1" in
+'-d'|'--delete') listBranches | xargs git branch --delete;;
+'-D') listBranches | xargs git branch --delete --force;;
+*) echo "ERROR: Unsupported flag: $1" >&2; exit 1;;
+esac
