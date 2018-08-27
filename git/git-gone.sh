249d54e6c3a39528ee5aa4d2b73527a09f1ead43
Petr Kozelka <pkozelka@gmail.com>
2018-08-27 11:28:07 +0200

    git-gone: use non-abbreviated output of hash; otherwise the size was not guaranteed

--- git/git-gone.sh
+++ git/git-gone.sh
@@ -1,1 +1,1 @@
-git branch -v| sed -n '/^  .* [0-9a-f]\{10\} \[gone\] /{s:  \([^[:space:]]*\) .*:\1:;p;}'
+git branch -v --no-abbrev | sed -n '/^  .* [0-9a-f]\{40\} \[gone\] /{s:  \([^[:space:]]*\) .*:\1:;p;}'
